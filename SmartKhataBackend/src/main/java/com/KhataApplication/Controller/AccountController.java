758036741906983f9262cd69a93d31e6bdce5153
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-12-15 14:46:28 +0530

    cors changes

--- SmartKhataBackend/src/main/java/com/KhataApplication/Controller/AccountController.java
+++ SmartKhataBackend/src/main/java/com/KhataApplication/Controller/AccountController.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
